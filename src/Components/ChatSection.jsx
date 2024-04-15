0b072a0c680a6b2c1b62abfe3d2cfa0498203d54
thilankadw <thilankawijesingham@gmail.com>
2024-04-15 11:15:01 +0530

    install tailwind

---
+++ src/Components/ChatSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ChatSection() {
+  return (
+    <div>
+        <div className='text-[100px] font-poppins'>
+            cvgfdgfvf
+        </div>
+    </div>
+  );
+}
