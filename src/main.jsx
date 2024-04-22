81df6ec6b98af8c2479d2a62b212b663c4b84bde
thilankadw <thilankawijesingham@gmail.com>
2024-04-22 19:17:41 +0530

    Revert "Merge branch 'main' into chatplatform"
    This reverts commit 8773f92fac4a1ef09ab8065b6edacb1ca6e6989e, reversing
    changes made to 999a3db1a343eaf38a1ef0de973c0ef0b01c553f.

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './features/store.js'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-    </BrowserRouter>
-   
   </React.StrictMode>
 )
